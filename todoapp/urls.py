9d59756c106d082d18f5721a9f6a47a08350676c
MangeshKokare <118895205+MangeshKokare@users.noreply.github.com>
2024-04-18 10:37:36 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+app_name = 'todoapp'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('todo/', views.todoappView, name='todo'),
+    path('addTodoItem/',views.addTodoView), 
+    path('deleteTodoItem/<int:id>/', views.deleteTodoView), 
+    path('editTodoItem/<int:id>/', views.editTodoView, name='edit_todo_item'),
+
+] 
+
